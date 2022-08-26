261e6150d877e6bf721a99fec70e6f4b45a5ca76
Mauricio Camacho <mcamachoxd@gmail.com>
2022-08-25 21:45:35 -0600

    Learning how the state work in React

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Square = (props) => {
+  const [valueButtonX, setvalueButonX] = useState();
+
   const ClickButtonHandler = () => {
+    setvalueButonX("X");
     console.log(`click in ${props.value}`);
   };
 
   return (
     <button className="square" onClick={ClickButtonHandler}>
-      {props.value}
+      {valueButtonX}
     </button>
   );
 };
