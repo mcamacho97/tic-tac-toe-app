3336ba5e17d0e11706203a81bd4e8c052a4765db
Mauricio Camacho <mcamacho9729@gmail.com>
2022-11-13 20:05:40 -0600

    Initial commit

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,13 @@
 import Board from "./components/Board";
+import { useState } from "react";
 
 const Game = () => {
+  const [history, setHistory] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  });
+
+
   return (
     <div className="game">
       <div className="game-board">
